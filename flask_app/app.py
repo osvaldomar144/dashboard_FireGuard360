ef2220e948e25c81513fcf0f860f3f6fabbd149b
MartinaLucianii <martina00.luciani@gmail.com>
2025-03-21 22:14:47 +0100

    correzione dei commenti

--- flask_app/app.py
+++ flask_app/app.py
@@ -8,9 +8,9 @@ def get_db_connection():
     conn = mysql.connector.connect(
         host="localhost",        # Host del contenitore MySQL
         user="root",             # Nome utente di default per MySQL
-        password="my-secret-pw", # La password che hai impostato
-        database="fireguard_db", # Il database che hai creato
-        port=3306               # La porta mappata su localhost (3307)
+        password="my-secret-pw", # password
+        database="fireguard_db", # database creato
+        port=3306               # La porta mappata su localhost (3306)
     )
     return conn
 
@@ -20,11 +20,11 @@ def index():
     conn = get_db_connection()
     cur = conn.cursor()
 
-    # Esegui una query per ottenere i dati (temperatura, umidità, fumo)
-    cur.execute('SELECT * FROM dati_arduino;')  # Puoi aggiungere altre query qui
-    dati = cur.fetchall()  # Ottieni tutti i dati dalla tabella
+    # Esecuzione della query per ottenere i dati (temperatura, umidità, fumo)
+    cur.execute('SELECT * FROM dati_arduino;')  
+    dati = cur.fetchall()  
 
-    # Chiudi la connessione
+    # Chiusura della connessione
     cur.close()
     conn.close()
 
