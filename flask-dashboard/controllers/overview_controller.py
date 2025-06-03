9472eaf851c830c731c1beea9d0b68e47b31038d
Alexis Martínez <osvaldomar144@gmail.com>
2025-06-03 13:02:34 +0200

    feat: added modular table macro (with mock data test) and fixed sidebar

--- flask-dashboard/controllers/overview_controller.py
+++ flask-dashboard/controllers/overview_controller.py
@@ -6,4 +6,18 @@ blp = Blueprint("overview", __name__, url_prefix='/overview')
 @blp.route('/')
 @login_required
 def index():
-    return render_template('overview/index.html')
+    # Esempio dati mock per test
+    alert_rows = [
+        {'Timestamp': '2025-06-01 14:23', 'Descrizione': 'Fumo rilevato nella Zona A'},
+        {'Timestamp': '2025-06-01 13:58', 'Descrizione': 'Temperatura anomala rilevata'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'}
+    ]
+    return render_template('overview/index.html', alert_rows=alert_rows)
