fc091d4c41fd827447dc59cb1a43778faad3c408
Alexis Martínez <osvaldomar144@gmail.com>
2025-06-03 14:31:47 +0200

    fix: table list elements, drone console box and added mock data for alerts history table

--- flask-dashboard/controllers/alerts_controller.py
+++ flask-dashboard/controllers/alerts_controller.py
@@ -6,4 +6,18 @@ blp = Blueprint("alerts", __name__, url_prefix='/alerts')
 @blp.route('/')
 @login_required
 def index():
-    return render_template('alerts/alerts.html')
+    # Esempio dati mock per test
+    alert_rows = [
+        {'Timestamp': '2025-06-01 14:23', 'Descrizione': 'Fumo rilevato nella Zona A'},
+        {'Timestamp': '2025-06-01 13:58', 'Descrizione': 'Temperatura anomala rilevata'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'},
+        {'Timestamp': '2025-06-01 13:10', 'Descrizione': 'Allarme manuale attivato'}
+    ]
+    return render_template('alerts/alerts.html', alert_rows=alert_rows)
