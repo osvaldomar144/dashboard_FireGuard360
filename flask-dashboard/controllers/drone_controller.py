33dddc77f49b135432a6880d5437a47df4b54228
Alexis Martínez <osvaldomar144@gmail.com>
2025-07-07 21:31:07 +0200

    feat: added drone handle on dashboard

--- flask-dashboard/controllers/drone_controller.py
+++ flask-dashboard/controllers/drone_controller.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from utility.db import exec_stored_procedure
 
 blp = Blueprint("drone", __name__, url_prefix='/drone')
 
 @blp.route('/')
 @login_required
 def index():
-    return render_template('drone/drone.html')
+    result = exec_stored_procedure("get_latest_system_danger", [1])
+    danger_level = result[0]['danger_level'] if result else 0
+    return render_template('drone/drone.html', danger_level=danger_level)
